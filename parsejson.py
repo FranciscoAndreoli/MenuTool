f899207368cf711a3cadd1567391abd56d6cd682
FranciscoAndreoli <72111673+FranciscoAndreoli@users.noreply.github.com>
2023-02-26 22:08:19 -0300

    Added taxes to the items

--- parsejson.py
+++ parsejson.py
@@ -11,67 +11,81 @@ class parsejson:
         def generate_UUID(): #Universally Unique Identifier
             return str(uuid.uuid4())
 
-        def name_section(): #gets menu section name
-            for i in range(0, len(datos['MenuSections'])):
-                yield datos['MenuSections'][i]['Name']
-
-        def enable_section(): #gets if menu section is enabled or not
-            for i in range(0, len(datos['MenuSections'])):
-                yield datos['MenuSections'][i]['IsAvailable']
-
         def store_name(): #gets store name
-            if datos["Name"] == None:
-                return None
-            else:
-                return datos['Name']
+            return datos['Name']
 
-        def notes_sections(): #gets description of menu sections
-            for i in range(0, len(datos["MenuSections"])):
-                if datos["MenuSections"][i]["Description"] == None or datos["MenuSections"][i]["Description"] == "":
-                    yield None
-                else:
-                    yield datos["MenuSections"][i]["Description"]
+        def get_tax(TaxRateId):
+            if TaxRateId == None:
+                return 0
+            elif TaxRateId in tax_dict:
+                return tax_dict[TaxRateId]
 
-        listNameSection = list(name_section())
-        listEnableSections = list(enable_section())
-        listNotes = list(notes_sections())
 
+        if len(datos["TaxRates"]) != 0:
+            tax_dict = {tax_rate["TaxRateId"]: tax_rate["Rate"] for tax_rate in datos["TaxRates"]}
+            print(tax_dict)
+            # Output example: {
+            #                   20505431: 5,
+            #                   20505432: 6,
+            #                   20505422: 7
+            #                 }
+        else:
+            pass
 
         my_dict = {
-                    "franchisorId": generate_UUID(),
-                    "id": generate_UUID(),
-                    "type": "Store",
-                    "name": store_name(),
-                    "notes": None,
-                    "categories": [
-
-                    ],
-                    "modifiers": []
-                }
-
-
-        for i in range(0, len(datos['MenuSections'])):
-            new_category = {
-                "id": "",
-                "caption": "",
-                "enabled": None,
-                "backgroundColor": None,
-                "foregroundColor": None,
-                "selectedBackgroundColor": None,
-                "selectedForegroundColor": None,
+                "franchisorId": generate_UUID(),
+                "id": generate_UUID(),
+                "type": "Store",
+                "name": store_name(),
                 "notes": None,
-                "items": [],
-                "overrides": []
+                "categories": [],
+                "modifiers": []
             }
 
-            new_category["id"] = generate_UUID()
-            new_category["caption"] = listNameSection[i]
-            new_category["enabled"] = listEnableSections[i]
-            new_category["notes"] = listNotes[i]
+        for section in datos['MenuSections']:
+            new_category = {
+            "id": generate_UUID(),
+            "caption": section['Name'],
+            "enabled": section['IsAvailable'],
+            "notes": section["Description"],
+            "items": [],
+            "overrides": []
+            }
+
+            for item in section['MenuItems']:
+
+                taxValue = get_tax(item["TaxRateId"])
+                booleano = False if item["TaxRateId"] is None else True
+
+                new_item = {
+                    "caption": item['Name'],
+                    "enabled": item['IsAvailable'],
+                    "id": generate_UUID(),
+                    "notes": item["Description"],
+                    "overrides": [],
+                    "pricingProfiles": [ {
+                        "collectionPrice": item['Price'],
+                        "collectionTax": taxValue,
+                        "collectionTaxable": booleano,
+                        "deliveryPrice": item['Price'],
+                        "deliveryTax": taxValue,
+                        "deliveryTaxable": booleano,
+                        "dineInPrice": item['Price'],
+                        "dineInTax": taxValue,
+                        "dineInTaxable": booleano,
+                        "priceBandId": "cc4efdb0-78a1-11ed-a7b2-713c0ffdd9d3",
+                        "takeawayPrice": item['Price'],
+                        "takeawayTax": taxValue,
+                        "takeawayTaxable": booleano
+                    }],
+                        "modifierMembers": [],
+                }
+
+                new_category["items"].append(new_item)
 
             my_dict["categories"].append(new_category)
 
-        #print(json.dumps(my_dict, indent=2))
+        print(json.dumps(my_dict, indent=2))
 
         # specify the path to save the file, including the desired name
         path = os.path.expanduser("~/Desktop/my_POS_JSON.json")
@@ -79,5 +93,7 @@ class parsejson:
         # open the file for writing, and save the dictionary as JSON
         with open(path, 'w') as outfile:
             json.dump(my_dict, outfile)
+
+
 
 
