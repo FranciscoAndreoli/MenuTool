976d93b1cf1033cb07604a6f1740b9dc9dfdfee2
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2023-02-27 12:14:20 -0300

    Principio de filtro Precio MO

--- parsejson.py
+++ parsejson.py
@@ -1,6 +1,7 @@
 import json
 import uuid
 import os
+
 class parsejson:
     def __init__(self):
         pass
@@ -9,10 +10,22 @@ class parsejson:
 
         def generate_UUID(): #Universally Unique Identifier
             return str(uuid.uuid4())
-
+        
         def store_name(): #gets store name
             return datos['Name']
-
+        
+        def get_tax(TaxRateId):
+            if TaxRateId == None:
+                return 0
+            elif TaxRateId in tax_dict:
+                return tax_dict[TaxRateId]
+        
+        if len(datos["TaxRates"]) != 0:
+            tax_dict = {tax_rate["TaxRateId"]: tax_rate["Rate"] for tax_rate in datos["TaxRates"]}
+        else:
+            pass
+        
+        
         my_dict = {
                 "franchisorId": generate_UUID(),
                 "id": generate_UUID(),
@@ -22,7 +35,7 @@ class parsejson:
                 "categories": [],
                 "modifiers": []
             }
-
+        
         for section in datos['MenuSections']:
             new_category = {
                 "id": generate_UUID(),
@@ -32,8 +45,13 @@ class parsejson:
                 "items": [],
                 "overrides": []
             }
-
+        
+        
             for item in section['MenuItems']:
+        
+                taxValue = get_tax(item["TaxRateId"])
+                booleano = False if item["TaxRateId"] is None else True
+        
                 new_item = {
                     "caption": item['Name'],
                     "enabled": item['IsAvailable'],
@@ -42,22 +60,22 @@ class parsejson:
                     "overrides": [],
                     "pricingProfiles": [ {
                         "collectionPrice": item['Price'],
-                        "collectionTax": 0,
-                        "collectionTaxable": False,
+                        "collectionTax": taxValue,
+                        "collectionTaxable": booleano,
                         "deliveryPrice": item['Price'],
-                        "deliveryTax": 0,
-                        "deliveryTaxable": False,
+                        "deliveryTax": taxValue,
+                        "deliveryTaxable": booleano,
                         "dineInPrice": item['Price'],
-                        "dineInTax": 0,
-                        "dineInTaxable": False,
-                        "priceBandId": 'cc4efdb0-78a1-11ed-a7b2-713c0ffdd9d3',
+                        "dineInTax": taxValue,
+                        "dineInTaxable": booleano,
+                        "priceBandId": "cc4efdb0-78a1-11ed-a7b2-713c0ffdd9d3",
                         "takeawayPrice": item['Price'],
-                        "takeawayTax": 0,
-                        "takeawayTaxable": False
-                    } ],
+                        "takeawayTax": taxValue,
+                        "takeawayTaxable": booleano
+                    }],
                     "modifierMembers": [],
                 }
-
+        
                 for modifier in item['MenuItemOptionSets']:
                     new_menuOptionSet = {
                         "caption": modifier['Name'],
@@ -65,16 +83,20 @@ class parsejson:
                         "modifierId": modifier['MenuItemOptionSetId'],
                         "overrides": []
                     }
-
+        
                     new_item["modifierMembers"].append(new_menuOptionSet)
-
+        
                 new_category["items"].append(new_item)
-
+        
             my_dict["categories"].append(new_category)
-
+        
+        
         for section in datos['MenuSections']:
             for item in section['MenuItems']:
+        
+        
                 for optionSet in item['MenuItemOptionSets']:
+        
                     new_optionSet = {
                         "canSameItemBeSelectedMultipleTimes": False,
                         "caption": optionSet['Name'],
@@ -86,8 +108,32 @@ class parsejson:
                         "overrides": [],
                         "items": []
                     }
-
+        
+                    def getMoPrices(optionSet):
+                        new_moPrices = []
+        
+                        if optionSet['IsMasterOptionSet'] == True:
+                            for moPrices in optionSet['MenuItemOptionSetItems']:
+                                new_moPrices.append(moPrices['Price'])
+        
+                        return new_moPrices
+        
+                    listMasterOptionPrices = getMoPrices(optionSet)
+        
+                    if listMasterOptionPrices:
+                        print('antes de filtrar', listMasterOptionPrices)
+                        lowestPrice =  min(listMasterOptionPrices)
+                        print('el mas bajo', lowestPrice)
+        
+                        for priceDifference in listMasterOptionPrices:
+                            newPrices = priceDifference - lowestPrice
+                            print('nuevo precio', newPrices)
+        
                     for option in optionSet['MenuItemOptionSetItems']:
+        
+                        taxValue = get_tax(option["TaxRateId"])
+                        booleano = False if option["TaxRateId"] is None else True
+        
                         new_option = {
                             "caption": option['Name'],
                             "enabled": option['IsAvailable'],
@@ -95,25 +141,25 @@ class parsejson:
                             "overrides": [],
                             "pricingProfiles": [ {
                                 "collectionPrice": option['Price'],
-                                "collectionTax": 0,
-                                "collectionTaxable": False,
+                                "collectionTax": taxValue,
+                                "collectionTaxable": booleano,
                                 "deliveryPrice": option['Price'],
-                                "deliveryTax": 0,
-                                "deliveryTaxable": False,
+                                "deliveryTax": taxValue,
+                                "deliveryTaxable": booleano,
                                 "dineInPrice": option['Price'],
-                                "dineInTax": 0,
-                                "dineInTaxable": False,
-                                "priceBandId": 'cc4efdb0-78a1-11ed-a7b2-713c0ffdd9d3',
+                                "dineInTax": taxValue,
+                                "dineInTaxable": booleano,
+                                "priceBandId": "cc4efdb0-78a1-11ed-a7b2-713c0ffdd9d3",
                                 "takeawayPrice": option['Price'],
-                                "takeawayTax": 0,
-                                "takeawayTaxable": False
+                                "takeawayTax": taxValue,
+                                "takeawayTaxable": booleano
                             } ],
                             "modifierMembers": []
                         }
-
+        
                         new_optionSet["items"].append(new_option)
-
-
+        
+        
                     my_dict["modifiers"].append(new_optionSet)
 
         #print(json.dumps(my_dict, indent=2))
